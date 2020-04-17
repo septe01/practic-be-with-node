38793dee24d166732c3f72e8aaa40629c1d7e657
septe.vhirgo@gmail.com <septe.vhirgo@gmail.com>
2020-04-17 10:28:12 +0700

    install group routes n setup cors

--- index.js
+++ index.js
@@ -2,12 +2,37 @@
 const express = require("express");
 //init body parser
 const bodyParser = require("body-parser");
+// init group route
+require("express-group-routes");
 // use express in variable
 const app = express();
+//cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 // define server port
 const port = 5000;
 //allow body parser
 app.use(bodyParser.json());
+
+// import controller
+const LoginController = require("./controllers/login");
+// -- create groups routes
+// -- Endpoint
+app.group("/api/v1", (routes) => {
+  //strukturing folder
+  routes.get("/", LoginController.index);
+});
+
+// app.group("/api/v1", (router) => {
+//   //using group routes
+//   router.get("/login", (req, res) => {
+//     res.send("hallo");
+//   });
+// });
 
 // create the homepage route
 app.get("/", (req, res) => {
