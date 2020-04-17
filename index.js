9036cc612cf322593a2f0e99b8e1599f52ef5bab
septe.vhirgo@gmail.com <septe.vhirgo@gmail.com>
2020-04-17 10:07:13 +0700

    bodyparser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // instants express module
 const express = require("express");
+//init body parser
+const bodyParser = require("body-parser");
 // use express in variable
 const app = express();
 // define server port
 const port = 5000;
+//allow body parser
+app.use(bodyParser.json());
 
 // create the homepage route
 app.get("/", (req, res) => {
