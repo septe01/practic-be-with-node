b48f12c2b224e098a7313e4e802d1be250ba7e43
septe.vhirgo@gmail.com <septe.vhirgo@gmail.com>
2021-04-29 06:14:34 +0700

    succes connect to mongoose

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // init group route
 require("express-group-routes");
+
+// mongoose import
+const mongooseDB = require('mongoose')
+const {MONGOURL} = require("./keys")
+
+mongooseDB.connect(MONGOURL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongooseDB.connection.on('connected',()=>{
+  console.log("connected to mongoose"); // look in the log
+})
+mongooseDB.connection.on('error',(err)=>{
+  console.log(`connected is ${err}`); // look in the log
+})
+
+
 // use express in variable
 const app = express();
 //handle cors
